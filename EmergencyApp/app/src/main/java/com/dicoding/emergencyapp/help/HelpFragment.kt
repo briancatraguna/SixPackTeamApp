912cb51d49b86d14e18b2aab14913c9fdaab4980
briancatraguna <briancatraguna@rocketmail.com>
2021-05-06 20:09:13 +0700

    Speech API worked

--- EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/help/HelpFragment.kt
+++ EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/help/HelpFragment.kt
@@ -6,6 +6,8 @@ import android.content.pm.PackageManager
 import android.media.MediaRecorder
 import android.os.Bundle
 import android.os.Environment
+import android.speech.RecognizerIntent
+import android.speech.SpeechRecognizer
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -19,42 +21,17 @@ import com.dicoding.emergencyapp.R
 import com.dicoding.emergencyapp.databinding.FragmentHelpBinding
 import com.dicoding.emergencyapp.home.HomeActivity
 import java.io.File
+import java.util.*
 import java.util.jar.Manifest
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HelpFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HelpFragment : Fragment() {
 
-    private var param1: String? = null
-    private var param2: String? = null
-
     private lateinit var binding: FragmentHelpBinding
-    private var state: Boolean = true
-    private lateinit var btnRecord: ImageView
-    private var path = Environment.getExternalStorageDirectory().toString()+"/record_data.3gp"
-    private lateinit var mediaRecorder : MediaRecorder
+    private val RQ_SPEECH_REC = 102
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == 111 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            btnRecord.isEnabled = true
-        }
     }
 
     override fun onCreateView(
@@ -62,55 +39,36 @@ class HelpFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentHelpBinding.inflate(inflater, container, false)
-        btnRecord = binding.imageView
         return binding.root
 
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        mediaRecorder = MediaRecorder()
-        if (ActivityCompat.checkSelfPermission(context as Activity, android.Manifest.permission.RECORD_AUDIO)  != PackageManager.PERMISSION_GRANTED) {
-            ActivityCompat.requestPermissions(context as Activity, arrayOf(android.Manifest.permission.RECORD_AUDIO, android.Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
-            btnRecord.setOnClickListener {
-                if(state == false) {
-                    mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC)
-                    mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP)
-                    mediaRecorder.setAudioEncoder(MediaRecorder.OutputFormat.AMR_NB)
-                    mediaRecorder.setOutputFile(path)
-                    mediaRecorder.prepare()
-                    mediaRecorder.start()
-                    state = true
-                    Toast.makeText(context, "Recording started!", Toast.LENGTH_LONG).show()
-                    Log.d("info", "$")
-                }
-                else if (state == true) {
-                    mediaRecorder.stop()
-                    state = false
-                    Toast.makeText(context, "Recording has stopped!", Toast.LENGTH_LONG).show()
-                }
-            }
+        val recordBtn = binding.recordButton
+        recordBtn.setOnClickListener {
+            askSpeechInput()
         }
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HelpFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HelpFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == RQ_SPEECH_REC && resultCode == Activity.RESULT_OK){
+            val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS)
+            binding.tvResult.text = result?.get(0).toString()
+        }
+    }
+
+    private fun askSpeechInput() {
+        if (!SpeechRecognizer.isRecognitionAvailable(context)){
+            Toast.makeText(context,"Speech recognition is not available",Toast.LENGTH_SHORT).show()
+        } else {
+            val i = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
+            i.putExtra(RecognizerIntent.EXTRA_LANGUAGE,Locale.getDefault())
+            i.putExtra(RecognizerIntent.EXTRA_PROMPT,"Explain your emergency situation!")
+            startActivityForResult(i,RQ_SPEECH_REC)
+        }
     }
 }
 
