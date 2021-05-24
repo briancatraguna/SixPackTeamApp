36071b543f8e54feb8deeae7c8716a5610d52d5e
briancatraguna <briancatraguna@rocketmail.com>
2021-05-24 21:07:16 +0700

    Authentication feature done

--- EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/ui/authentication/signup/SignUpActivity.kt
+++ EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/ui/authentication/signup/SignUpActivity.kt
@@ -1,10 +1,16 @@
 package com.dicoding.emergencyapp.ui.authentication.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import com.dicoding.emergencyapp.R
 import com.dicoding.emergencyapp.databinding.ActivitySignUpBinding
+import com.dicoding.emergencyapp.ui.authentication.login.LoginActivity
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.firebase.auth.AuthResult
+import com.google.firebase.auth.FirebaseAuth
 
 class SignUpActivity : AppCompatActivity(),View.OnClickListener {
 
@@ -17,6 +23,7 @@ class SignUpActivity : AppCompatActivity(),View.OnClickListener {
 
         binding.buttonBack.setOnClickListener(this)
         binding.tvLogin.setOnClickListener(this)
+        binding.buttonSignup.setOnClickListener(this)
 
     }
 
@@ -29,6 +36,32 @@ class SignUpActivity : AppCompatActivity(),View.OnClickListener {
 
             R.id.tv_login -> {
                 finish()
+            }
+
+            R.id.button_signup -> {
+                val email = binding.edittextEmail.text.toString().trim()
+                val password = binding.edittextPassword.text.toString().trim()
+                val confirmPassword = binding.edittextConfirmPassword.text.toString().trim()
+                if (email.equals("")){
+                    binding.edittextEmail.error = "Enter email"
+                } else if (password.equals("")){
+                    binding.edittextPassword.error = "Enter password"
+                } else if (confirmPassword.equals("")){
+                    binding.edittextConfirmPassword.error = "Confirm password"
+                } else if (!confirmPassword.equals(password)){
+                    binding.edittextConfirmPassword.error = "Password mismatched"
+                } else {
+                    val mAuth: FirebaseAuth = FirebaseAuth.getInstance()
+                    mAuth.createUserWithEmailAndPassword(email,password)
+                        .addOnCompleteListener(this, OnCompleteListener<AuthResult>(){
+                            if (it.isSuccessful){
+                                Toast.makeText(this,"Account succesfully registered",Toast.LENGTH_SHORT).show()
+                                finish()
+                            } else {
+                                Toast.makeText(this,"Account failed to be registered: ${it.exception}",Toast.LENGTH_SHORT).show()
+                            }
+                        })
+                }
             }
         }
     }
