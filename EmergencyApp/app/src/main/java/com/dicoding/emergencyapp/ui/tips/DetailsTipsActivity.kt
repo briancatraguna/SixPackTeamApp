456b5c6ce5812e68c259eb4377abbf8abcfa8134
briancatraguna <briancatraguna@rocketmail.com>
2021-05-28 21:55:01 +0700

    VideoView worked

--- EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/ui/tips/DetailsTipsActivity.kt
+++ EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/ui/tips/DetailsTipsActivity.kt
@@ -1,14 +1,19 @@
 package com.dicoding.emergencyapp.ui.tips
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.MediaController
+import android.widget.Toast
 import com.dicoding.emergencyapp.data.local.TipsLocalDataSource
 import com.dicoding.emergencyapp.data.repository.TipsRepository
 import com.dicoding.emergencyapp.databinding.ActivityDetailsTipsBinding
+import kotlinx.android.synthetic.main.activity_details_tips.*
 
 class DetailsTipsActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityDetailsTipsBinding
+    private var mediaController: MediaController? = null
 
     companion object{
         const val TOPIC_KEY = "topic_key"
@@ -33,10 +38,20 @@ class DetailsTipsActivity : AppCompatActivity() {
     private fun populateView() {
         val viewModel = TipsViewModel(TipsRepository.getInstance(TipsLocalDataSource(this)))
         val key = intent.getStringExtra(TOPIC_KEY)
+        val videoView = binding.videoView
+        if (mediaController == null){
+            mediaController = MediaController(this)
+            mediaController!!.setAnchorView(this.videoView)
+        }
+        videoView.setMediaController(mediaController)
         if (key!=null){
             val tips = viewModel.getTips(key)
             binding.toolbar.tvTitle.text = tips?.title
             binding.tvDescription.text = tips?.description
+            val uri = "android.resource://"+packageName+"/"+tips?.video
+            videoView!!.setVideoURI(Uri.parse(uri))
+            videoView!!.requestFocus()
+            videoView!!.start()
         }
     }
 }
