12ea5a87f0dd6baa927b5aa2e30bded93f02aac4
briancatraguna <briancatraguna@rocketmail.com>
2021-06-02 01:16:28 +0700

    Connection to firebase worked

--- ResponderApp/app/src/main/java/com/bangkit/responderapp/MainActivity.kt
+++ ResponderApp/app/src/main/java/com/bangkit/responderapp/MainActivity.kt
@@ -3,6 +3,11 @@ package com.bangkit.responderapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bangkit.responderapp.databinding.ActivityMainBinding
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +17,26 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        
+    }
+
+    private fun testFirebase(){
+        val database = Firebase.database
+        val myRef = database.getReference("users")
+        myRef.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                if (snapshot.exists()){
+                    val snapshotChildren = snapshot.children
+                    for (user in snapshotChildren){
+                        println(user.key)
+                    }
+                }
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+        })
     }
 }
