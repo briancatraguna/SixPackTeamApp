735d48f3ebcc50e9b8b5e191ce7c29906993e5fc
briancatraguna <briancatraguna@rocketmail.com>
2021-06-01 16:42:26 +0700

    Firebase realtime db officialy worked, next is reading from db

--- EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/ui/sos/SosViewModel.kt
+++ EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/ui/sos/SosViewModel.kt
@@ -4,55 +4,28 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.dicoding.emergencyapp.data.entity.PostResponse
+import com.dicoding.emergencyapp.data.repository.FirebaseRepository
 import com.dicoding.emergencyapp.data.retrofit.SheetRetrofitClient
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SosViewModel: ViewModel() {
-    private val _status = MutableLiveData<String>()
-    fun getStatus(): LiveData<String> = _status
+class SosViewModel(private val repository: FirebaseRepository): ViewModel() {
 
-    fun postData(
-            userId: String?,
-            userPhoto: String?,
-            transcription: String,
-            report: String,
-            latitude: Double?,
-            longitude: Double?,
-            status: String
-    ){
-        SheetRetrofitClient.instance
-            .postData(userId,userPhoto,transcription,report,latitude,longitude,status)
-            .enqueue(object : Callback<PostResponse>{
-                override fun onResponse(
-                    call: Call<PostResponse>,
-                    response: Response<PostResponse>
-                ) {
-                    if (response.isSuccessful){
-                        _status.postValue(response.body()?.status)
-                    } else {
-                        _status.postValue("Failed to send data")
-                    }
-                }
-
-                override fun onFailure(call: Call<PostResponse>, t: Throwable) {
-                    _status.postValue("Failed to send data")
-                }
-            })
+    fun getStatus(): LiveData<Boolean>{
+        return repository.getUploadStatus()
     }
 
-    fun addToDatabase(
-            userId: String?,
-            userPhoto: String?,
-            transcription: String,
-            report: String,
-            latitude: Double?,
-            longitude: Double?,
-            status: String
-        ){
-
-
+    fun uploadData(
+        timestamp: String,
+        userId: String?,
+        transcription: String,
+        report: String,
+        latitude: Double?,
+        longitude: Double?,
+        status: String
+    ){
+        repository.uploadData(timestamp,userId,transcription,report,latitude,longitude,status)
     }
 
 }
