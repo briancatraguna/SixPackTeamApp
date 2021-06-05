7c63e911ada042f87302a080e87cb41aab1cb11d
briancatraguna <briancatraguna@rocketmail.com>
2021-06-05 12:45:41 +0700

    Connected adapter to data from firebase

--- ResponderApp/app/src/main/java/com/bangkit/responderapp/ui/MainActivity.kt
+++ ResponderApp/app/src/main/java/com/bangkit/responderapp/ui/MainActivity.kt
@@ -2,6 +2,10 @@ package com.bangkit.responderapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.bangkit.responderapp.data.FirebaseDataSource
+import com.bangkit.responderapp.data.FirebaseRepository
 import com.bangkit.responderapp.databinding.ActivityMainBinding
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -12,10 +16,12 @@ import com.google.firebase.ktx.Firebase
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        viewModel = MainViewModel(FirebaseRepository(FirebaseDataSource()))
         setContentView(binding.root)
 
         populateView()
@@ -23,6 +29,19 @@ class MainActivity : AppCompatActivity() {
 
     private fun populateView() {
         val rvReports = binding.rvReports
+        rvReports.layoutManager = LinearLayoutManager(this)
+        val listReportsAdapter = ListReportsAdapter(this)
+        viewModel.getAllReports().observe(this,{reports->
+            listReportsAdapter.setData(reports)
+            rvReports.adapter = listReportsAdapter
+        })
 
+        viewModel.readSuccess().observe(this,{success->
+            if (!success){
+                binding.tvFail.visibility = View.VISIBLE
+            } else {
+                binding.tvFail.visibility = View.GONE
+            }
+        })
     }
 }
