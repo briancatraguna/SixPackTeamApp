c897fac8b6a08a0d0f64a5a7bee80bdc2963c837
Annisa Nuri Nabila <73707695+annisann@users.noreply.github.com>
2021-06-07 16:40:20 +0700

    fix bugs
    - refactoring
    - update test
    - fix loadModel method
    - fix train method

--- ner/ner.py
+++ ner/ner.py
@@ -1,15 +1,20 @@
-import numpy as np
-import pandas as pd
-import os
 import glob
 import json
+import nltk
+import os
+import numpy as np
+import pandas as pd
 import tensorflow as tf
 import tensorflow_hub as hub
-from contextlib import redirect_stdout
 from tensorflow import keras
+from contextlib import redirect_stdout
+from nltk.tokenize import word_tokenize
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.utils import plot_model
+
+# download punctuation from nltk
+nltk.download('punkt')
 
 class Data:
     def __init__(self, trainData, testData):
@@ -18,8 +23,8 @@ class Data:
         Args: 
             trainData & testData: csv files.
         '''
-        self.trainData = pd.read_csv(trainData)[:21200].append(pd.read_csv(trainData)[26485:32445])
-        self.testData = pd.read_csv(testData)[:2000]
+        self.trainData = pd.read_csv(trainData)[:44527]
+        self.testData = pd.read_csv(testData)[:2132]
         self.token = self.trainData.token
         self.tag = self.trainData.tag
         self.tagList = list(self.tag.unique())
@@ -61,56 +66,54 @@ class Data:
                                                                           self.lenY)
 
 class Model:
-    def __init__(self, X, Y):
+    def __init__(self, tag):
         '''
-        Initialize class attributes.
+        Initialize class.
         Args:
-            X: list of tokens.
-            Y: list of tags.
+            tag: dictionary of the tag/label
         '''
-        self.X = X
-        self.Y = Y
+        self.tag = tag  # tag dictionary
+
         self.MODEL_PATH = 'model/'
         self.embedding = "https://tfhub.dev/google/nnlm-id-dim50-with-normalization/2"
         self.optimizer ="rmsprop"
         self.loss = "sparse_categorical_crossentropy"
         self.metrics = ["accuracy"]
-        self.nOutput = len(set(self.Y))
-        self.model = self.layer()
-
+        
         self.has_trained = False
 
-    def layer(self):
-        '''
-        Keras layers.
-        '''
-        self.model = Sequential([hub.KerasLayer(self.embedding,
-                                                input_shape=[],
-                                                dtype=tf.string,
-                                                trainable=True),
-                                Dense(128, activation='relu'),
-                                # Dense(64, activation='relu'),
-                                Dense(self.nOutput, activation='softmax')
-                                ])
-        return self.model
-
-    def train(self, epochs, verbose):
+    def train(self, X, Y, epochs, verbose):
         '''
         A method to train the model.
         Args:
+            X: list of tokens.
+            Y: list of tags.
             epochs: number of epoch.
             verbose: number of verbose.
         '''
         self.has_trained = True
+
+        self.X = X
+        self.Y = Y
+        self.nOutput = len(set(self.Y)) # buat train
+
         self.epochs = epochs
         self.verbose = verbose
+        self.model = Sequential([hub.KerasLayer(self.embedding,
+                                                input_shape=[],
+                                                dtype=tf.string,
+                                                trainable=True),
+                                  Dense(128, activation='relu'),
+                                  Dense(64, activation='relu'),
+                                  Dense(self.nOutput, activation='softmax')
+                                  ])
         self.model.compile(optimizer=self.optimizer, loss=self.loss, metrics=self.metrics)
-        return self.model.fit(self.X, self.Y, self.epochs, self.verbose)
+        self.model.fit(self.X, self.Y, self.epochs, self.verbose)
 
     def save(self):
         '''
         Save the trained model into each folder.
-        Contain its configuration, architecture summary, keras file, and model plot.
+        Contain its configuration, architecture summary, weights, and model plot.
         '''
         def getLastIndex(self):
             folder = [f for f in glob.iglob(self.MODEL_PATH+'run_*')]
@@ -122,7 +125,7 @@ class Model:
             os.makedirs(self.workingDir)
 
             with open(os.path.join(self.workingDir, 'model_config.json'), 'w') as fw : 
-                json.dump(self.layer().get_config(), fw)
+                json.dump(self.model.get_config(), fw)
 
             with open(os.path.join(self.workingDir, 'architecture.txt'),'w+') as f:
                 with redirect_stdout(f):
@@ -136,35 +139,37 @@ class Model:
     def loadModel(self, processID):
         '''
         Load model from trained model folder.
+        Args:
+          processID: the number of process you want to load. see `model/run_*`
         '''
-        self.path = self.MODEL_PATH +'run_'+str(self.processID)
+        self.path = self.MODEL_PATH +'run_'+str(processID)
+
         with open(os.path.join(self.path, 'model_config.json'), 'r') as json_file:
             json_file = json.load(json_file)
-        self.model = Sequential.from_config((json_file))
-        self.model.load_weights(os.path.join(self.path, 'model_weight.h5'))
-        return self.model
+            
+        self.model = Sequential.from_config(json_file, custom_objects={'KerasLayer':hub.KerasLayer})
+        self.model.load_weights(os.path.join(self.path, 'model.h5'))
+        print('Model loaded.')
 
-    def test(self, testData, tag):
+    def test(self, data):
         '''
         Test the model with test set.
         Args:
             testData: csv file of test dataset.
             tag     : tag index.
         '''
-        self.testData = testData
-        self.tag = tag
+        self.data = data
+        
+        if type(self.data) is pd.DataFrame:
+            self.word = list(self.data.token)
+        elif type(self.data) is str:
+            self.word = word_tokenize(self.data)
 
-        if self.has_trained == True:
-            self.Xtest = list(self.testData.token)
-            self.labelOutput = {index: tag for index, tag in enumerate(self.tag)}
-
-            for i in range(len(self.Xtest)):
-                pred = self.model.predict(self.Xtest)[i]
-                labelIndex = np.argmax(pred)
-                print('{:20} {}'.format(self.Xtest[i], self.labelOutput.get(labelIndex)))
-        else:
-            print('Train model first.')
-
+        self.labelOutput = {index: tag for index, tag in enumerate(self.tag)}
+        for i in range(len(self.word)):
+            pred = self.model.predict(self.word)[i]
+            labelIndex = np.argmax(pred)
+            print('{:20} {}'.format(self.word[i], self.labelOutput.get(labelIndex)))
 
 if __name__ == '__main__': 
     load = Data('train.csv', 'test.csv')
@@ -172,7 +177,7 @@ if __name__ == '__main__':
     testData = load.testData
     tag = load.getTagIndex()
     
-    model = Model(x, y)
-    model.train()
+    model = Model(tag)
     model.save()
-    model.test(testData, tag)
+    model.loadModel(1)
+    model.test(testData)
