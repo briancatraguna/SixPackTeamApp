76c5bef3f4106adac72e1a2c02fe0f662fa3fecf
briancatraguna <briancatraguna@rocketmail.com>
2021-06-08 23:45:32 +0700

    Finished string parser

--- EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/helpers/NerResponseParser.kt
+++ EmergencyApp/app/src/main/java/com/dicoding/emergencyapp/helpers/NerResponseParser.kt
@@ -5,6 +5,23 @@ import com.dicoding.emergencyapp.data.entity.NerResponse
 class NerResponseParser(private val response: NerResponse) {
 
     fun getStringFormat(): String{
-        return ""
+        val locationList = response.lOCATION
+        val personList = response.pERSON
+        val descriptionList = response.dESCRIPTION
+        val weaponList = response.wEAPON
+        val locationListString: String = "Location: ${locationList?.joinToString()}"
+        val personListString: String = "Persons: ${personList?.joinToString()}"
+        val descriptionListString: String = "Descriptions: ${descriptionList?.joinToString()}"
+        val weaponListString: String = "Weapons involved: ${weaponList?.joinToString()}"
+
+        val result = """
+            ${locationListString}
+            ${personListString}
+            ${descriptionListString}
+            ${weaponListString}
+        """.trimIndent()
+
+
+        return result
     }
 }
